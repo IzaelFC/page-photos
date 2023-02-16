7042925e6d7bfe49d923ef8a135dc67ff4713431
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2023-02-16 17:15:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 async function main(){
     var response =  await(await fetch`https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=20`).json()
-    console.log(response[0]['links'])
 
     var imagem1 = response[0]['links']['download']
     var imagem2 = response[1]['links']['download']
@@ -10,9 +9,11 @@ async function main(){
 
     var lista = [imagem1, imagem2, imagem3, imagem4, imagem5]
 
-    document.getElementById('imagem1').src = lista[0]
-    document.getElementById('imagem2').src = lista[1]
-    document.getElementById('imagem3').src = lista[2]
-    document.getElementById('imagem4').src = lista[3]
-    document.getElementById('imagem5').src = lista[4]
+    document.getElementById('imagem1').src = lista[0], document.getElementById('id_imagem1').href = lista[0]
+    document.getElementById('imagem2').src = lista[1], document.getElementById('id_imagem1').href = lista[1]
+    document.getElementById('imagem3').src = lista[2], document.getElementById('id_imagem1').href = lista[2]
+    document.getElementById('imagem4').src = lista[3], document.getElementById('id_imagem1').href = lista[3]
+    document.getElementById('imagem5').src = lista[4], document.getElementById('id_imagem1').href = lista[4]
 }
+
+main()
