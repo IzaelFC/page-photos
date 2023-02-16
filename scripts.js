3a92b4a0ac1479833fcbcf008808cf7d7548c1c3
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2023-02-16 15:58:23 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+async function main(){
+    var response =  await(await fetch`https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=20`).json()
+    console.log(response[0]['links'])
+
+    var imagem1 = response[0]['links']['download']
+    var imagem2 = response[1]['links']['download']
+    var imagem3 = response[2]['links']['download']
+    var imagem4 = response[3]['links']['download']
+    var imagem5 = response[4]['links']['download']
+
+    var lista = [imagem1, imagem2, imagem3, imagem4, imagem5]
+
+    document.getElementById('imagem1').src = lista[0]
+    document.getElementById('imagem2').src = lista[1]
+    document.getElementById('imagem3').src = lista[2]
+    document.getElementById('imagem4').src = lista[3]
+    document.getElementById('imagem5').src = lista[4]
+}
