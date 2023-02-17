68909aa09ca06c3361768b4066c61f76b870283b
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2023-02-17 17:35:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 async function main() {
     const lista = await response();
   
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 12; i++) {
       const img = document.getElementById(`imagem${i}`);
       img.src = lista[i - 1];
       const link = document.getElementById(`id_imagem${i}`);
@@ -11,10 +11,10 @@ async function main() {
   
   async function response() {
     const response = await fetch(
-      `https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=20`
+      `https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=15`
     );
     const data = await response.json();
-    const lista = data.slice(0, 5).map((item) => item.links.download);
+    const lista = data.slice(0, 12).map((item) => item.links.download);
     return lista;
   }
 
