a0467875a709c5989f0bc9200b1c80174e80f769
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2023-02-17 15:05:28 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,21 @@
-async function main(){
-    var response =  await(await fetch`https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=20`).json()
+async function main() {
+    const lista = await response();
+  
+    for (let i = 1; i <= 5; i++) {
+      const img = document.getElementById(`imagem${i}`);
+      img.src = lista[i - 1];
+      const link = document.getElementById(`id_imagem${i}`);
+      link.href = lista[i - 1];
+    }
+  }
+  
+  async function response() {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random/?client_id=0Duyq_qOF6ED6_3N6zZSodSqTb45i09-2QhC-1isuQc&count=20`
+    );
+    const data = await response.json();
+    const lista = data.slice(0, 5).map((item) => item.links.download);
+    return lista;
+  }
 
-    var imagem1 = response[0]['links']['download']
-    var imagem2 = response[1]['links']['download']
-    var imagem3 = response[2]['links']['download']
-    var imagem4 = response[3]['links']['download']
-    var imagem5 = response[4]['links']['download']
-
-    var lista = [imagem1, imagem2, imagem3, imagem4, imagem5]
-
-    document.getElementById('imagem1').src = lista[0]
-    document.getElementById('id_imagem1').href = lista[0]
-    
-    document.getElementById('imagem2').src = lista[1]
-    document.getElementById('id_imagem1').href = lista[1]
-    
-    document.getElementById('imagem3').src = lista[2]
-    document.getElementById('id_imagem1').href = lista[2]
-    
-    document.getElementById('imagem4').src = lista[3]
-    document.getElementById('id_imagem1').href = lista[3]
-    
-    document.getElementById('imagem5').src = lista[4]
-    document.getElementById('id_imagem1').href = lista[4]
-}
-
-main()
+  main()
